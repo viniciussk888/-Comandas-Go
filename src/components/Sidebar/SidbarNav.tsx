8296b7c1874a9c6f1c92167bd228ffc600750e5a
vinicius <vinicius.cross07@gmail.com>
2022-02-16 10:16:21 -0300

    feature/reactor

--- src/components/Sidebar/SidbarNav.tsx
+++ src/components/Sidebar/SidbarNav.tsx
@@ -1,5 +1,5 @@
 import { Icon, Link, Stack, Text } from "@chakra-ui/react";
-import { RiContactsBookLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+import { RiShoppingCart2Line,RiFileList3Line, RiFileCopy2Line, RiHandCoinLine } from "react-icons/ri";
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
@@ -7,12 +7,13 @@ export function SidebarNav () {
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="GERAL">
-        <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
-        <NavLink icon={RiContactsBookLine} href="/users">Contatos</NavLink>
+        <NavLink icon={RiFileCopy2Line} href="/comanda">Comandas</NavLink>
+       {/*  <NavLink icon={RiFileCopy2Line} href="/dashboard">Dashboard</NavLink> */}
+        <NavLink icon={RiShoppingCart2Line} href="/products">Produtos</NavLink>
       </NavSection>
-      <NavSection title="AUTOMAÇÂO">
-        <NavLink icon={RiInputMethodLine} href="/forms">Formulários</NavLink>
-        <NavLink icon={RiGitMergeLine} href="/automation">Automação</NavLink>
+      <NavSection title="FINANCEIRO">
+        <NavLink icon={RiHandCoinLine} href="/caixa">Caixa</NavLink>
+        <NavLink icon={RiFileList3Line} href="/reports">Relatórios</NavLink>
       </NavSection>
     </Stack>
   )
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Stack, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
 import Link from 'next/link'
-import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { RiAddLine, RiPencilLine, RiDeleteBin3Line } from 'react-icons/ri'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { SideBar } from '../../components/Sidebar'
@@ -20,9 +20,9 @@ export default function Home() {
 
         <Box flex="1" borderRadius="8" bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">Produtos</Heading>
 
-            <Link href="/users/create" passHref>
+            <Link href="/products/create" passHref>
               <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
                 Criar novo
               </Button>
@@ -35,8 +35,8 @@ export default function Home() {
                 <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
-                <Th>Usuários</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
+                <Th>Nome</Th>
+                { isWideVersion && <Th>Valor</Th> }
                 <Th></Th>
               </Tr>
             </Thead>
@@ -46,12 +46,13 @@ export default function Home() {
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td px={["4", "4", "6"]}>
-                  <Text colorScheme="bold">Vinicius Martins</Text>
+                  <Text colorScheme="bold">Cerveja Skoll 600ml</Text>
                   <Text fontSize="sm" color="gray.300">vinicius.cross07@gmail.com</Text>
                 </Td>
-                { isWideVersion && <Td px={["4", "4", "6"]}>16 de Fevereiro, 2022</Td> }
+                { isWideVersion && <Td px={["4", "4", "6"]}>R$ 8,00</Td> }
                 <Td px={["4", "4", "6"]}>
                   <Button
+                  mr='3'
                     as="a"
                     size="sm"
                     fontSize="sm"
@@ -59,6 +60,46 @@ export default function Home() {
                     leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
                     Editar
+                  </Button>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="red"
+                    leftIcon={<Icon as={RiDeleteBin3Line} fontSize="16" />}
+                  >
+                    Excluir
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td px={["4", "4", "6"]}>
+                  <Text colorScheme="bold">Tabua de frios 4P</Text>
+                  <Text fontSize="sm" color="gray.300">Queijo, Palmito, Salame, Presunto e salgados.</Text>
+                </Td>
+                { isWideVersion && <Td px={["4", "4", "6"]}>R$ 40,00</Td> }
+                <Td px={["4", "4", "6"]}>
+                  <Button
+                  mr='3'
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="red"
+                    leftIcon={<Icon as={RiDeleteBin3Line} fontSize="16" />}
+                  >
+                    Excluir
                   </Button>
                 </Td>
               </Tr>
