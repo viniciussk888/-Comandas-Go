380613d8449bf9c75c08a2c2ab2a68b6bab6f339
vinicius <vinicius.cross07@gmail.com>
2022-04-06 08:21:48 -0300

    feature:supabase-key

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://ktltjynbnxeischkkluv.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0bHRqeW5ibnhlaXNjaGtrbHV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDUwNDM1NzcsImV4cCI6MTk2MDYxOTU3N30.4Zt4V2AYYZN_oNjrRhzzuIMijMzuzGGUyt7EQ7zIN7A'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
